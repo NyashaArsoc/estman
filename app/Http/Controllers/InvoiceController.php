742792afbfc25cc6363f676b7a01db8915cd2111
marcos <marcos@gmail.com>
2023-11-29 09:07:09 +0200

    UI for preinvoice list

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\invoice;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class InvoiceController extends Controller
+{
+    private $currentperiod;
+    private $previousperiod;
+    private $nextperiod;
+
+    public function __construct(){
+
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(invoice $invoice)
+    {
+        //
+    }
+
+    public function compilepreinvoice(){
+        try {
+            $arr= DB::select('EXEC spPostpreinvoice');
+            if($arr){
+                $result = $arr[0]->ReturnValue;
+                if($result ==0){
+                    //success full run 
+                    return 'success run'.$result;
+                }else{
+                    //already run 
+                    return 'you can only run once'.$result;
+                }
+            }else{
+               return 'problem in connection';
+            }
+        } catch (QueryException $th) {
+            return 'failed to execute query';
+        }
+        
+    }
+public function listpreinvoice(){
+    return view('invoice/pre-invoice-list');
+}
+}
---
+++ resources/views/invoice/pre-invoice-list.blade.php
@@ -0,0 +1,70 @@
+@php $title = 'Pending Approval'; 
+      $description = 'properties pending approval...'; @endphp
+    @extends('layout.main-layout')
+    @section('title', 'Property Approval')
+    @section('additional css')
+    <!-- Additional css Start-->
+    <link rel="stylesheet" href="{{ asset('css/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/select2-bootstrap-theme/select2-bootstrap.min.css') }}">
+    <!-- Additional css End-->
+    @endsection
+    @section('content')
+        <!-- Content Start-->
+        <div class="container-fluid">
+            <h4>{{$title}}</h4>
+            <ol class="breadcrumb no-bg mb-1">
+                <li class="breadcrumb-item"><a href="">Dashboard</a></li>
+                <li class="breadcrumb-item active">{{$title}}</li>
+            </ol>
+            <div class="box box-block bg-white">
+                <h5>{{$title}}</h5>
+                <p class="font-90 text-muted mb-1"> {{$description}}</p>
+                <div class="table-responsive">
+                    <hr/><table  class="datatable table table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Period</th>
+                                    <th>Currency</th>
+                                    <th>Total Amount</th>
+                                    <th>Option</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        <a class="btn btn-info btn-sm " id=""
+                                        href=""
+                                        title="view"><i class="ti-eye mr-0-5"></i>view</a>
+                            </td>
+                            </tr>
+                            
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Period</th>
+                                    <th>Currency</th>
+                                    <th>Total Amount</th>
+                                    <th>Option</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                        @include('layout.arlet')
+                </div>
+            </div>
+        </div>
+        <!-- Content End-->
+    @endsection
+    @section('additional js')
+    <!-- Additional JS Start-->
+    <script src="{{ asset('css/select2/select2.min.js') }}"></script>
+        <script src="{{ asset('js/select2.js') }}"></script>
+    <!-- Additional JS End-->
+    @endsection
