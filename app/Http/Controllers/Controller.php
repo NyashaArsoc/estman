9fe10f4583c7ed2fa4952d56601abdeb876a9c1e
marcos <marcos@gmail.com>
2024-01-16 20:01:34 +0200

    call system date

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function systemdate(){
+        try {
+            
+            $sysdate   = DB::table('sysdates')
+            ->select('*')->where('OpenClose','=','O')
+            ->latest('id')->first();
+            if(is_null($sysdate)){
+                return 'failed';
+            }else{
+                return $sysdate->systemdate;
+            }
+        } catch (QueryException $th) {
+           return 'failed';
+        }
+       
+    }
 }
