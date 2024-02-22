86115fed191ee5661b78a3ba5a2dc8931f54144a
marcos <marcos@gmail.com>
2024-02-22 19:38:30 +0200

    add error pages

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.login-layout')
+@section('title', '402')
+@section('content')
+<div class="container-fluid">
+        <div class="sign-form">
+            <div class="row">
+                <div class="col-md-4 offset-md-4 px-3">
+                    <div class="box b-a-0">
+                        <div class="p-2 text-xs-center">
+                            <h3>PAYMENT REQUIRED</h3> 
+                            <a class="btn btn-warning btn-sm "  href="{{route('dash.property')}}"
+                                title="back"><i class="ti-back-left mr-0-5"></i>back</a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
