9da11544ee9b59393496da0d99377349bd862093
marcos <marcos@gmail.com>
2024-02-23 23:27:54 +0200

    favicon change

--- resources/views/layout/view-btn-menu-layout.blade.php
+++ resources/views/layout/view-btn-menu-layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <!-- Meta tags -->
     <meta charset="utf-8">
+    <link rel="icon" type="image/svg" href="{{ asset('img/top.svg') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="">
