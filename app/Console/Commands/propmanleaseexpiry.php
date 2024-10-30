af411698f7ef63e4bc177a7b1a9771a2a2d80411
marcos <marcos@gmail.com>
2024-10-30 20:14:33 +0200

    run task to expire lease

---
+++ app/Console/Commands/propmanleaseexpiry.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class propmanleaseexpiry extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:propmanleaseexpiry';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Terminate lease expired';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+        DB::table('propmanlease')->where('available','=','Y')
+                ->where('validto','<',now())
+                ->update(['expiry' => 'Y' , 'available'=> 'N']);
+    }
+}
