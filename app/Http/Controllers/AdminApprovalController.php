4e1780a8421ec12eb46841399691742b10d8cc81
Shania-art <snyaude@arsoc.co.zw>
2025-10-02 05:13:37 +0200

    Approval

--- app/Http/Controllers/AdminApprovalController.php
+++ app/Http/Controllers/AdminApprovalController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class AdminApprovalController extends Controller
 {
-    //
+    public function requisitionApproval()
+{
+    return view('admin.approval.requisition-approval');
+}
+
+public function requisitionView()
+{
+    return view('admin.approval.requisition-view');
+}
+
 }
