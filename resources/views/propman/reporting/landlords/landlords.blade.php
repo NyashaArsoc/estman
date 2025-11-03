9e87f02247ff812dce369c3f31f5e7ede0d060aa
Shania-art <snyaude@arsoc.co.zw>
2025-11-03 09:15:32 +0200

    landlords

---
+++ resources/views/propman/reporting/landlords/landlords.blade.php
@@ -0,0 +1,44 @@
+@php
+$title = 'Landlord Report';
+@endphp
+
+@extends('layout.propman-main-menu')
+
+@section('title', $title)
+
+@section('content')
+<div class="container-fluid">
+  <h4>{{ $title }}</h4>
+
+  <form method="GET" action="{{ route('prop.report.landlord.generate') }}">
+    <div class="row">
+      <div class="col-md-3">
+        <label for="date_from">Date From</label>
+        <input type="date" name="date_from" id="date_from" class="form-control" required>
+      </div>
+      <div class="col-md-3">
+        <label for="date_to">Date To</label>
+        <input type="date" name="date_to" id="date_to" class="form-control" required>
+      </div>
+      <div class="col-md-3">
+        <label for="landlord_type">Landlord Type</label>
+        <select name="landlord_type" id="landlord_type" class="form-control" required>
+          <option value="all">All</option>
+          <option value="individual">Individual</option>
+          <option value="corporate">Corporate</option>
+        </select>
+      </div>
+      <div class="col-md-3">
+        <label for="format">Export Format</label>
+        <select name="format" id="format" class="form-control" required>
+          <option value="pdf">PDF</option>
+          <option value="excel">Excel</option>
+        </select>
+      </div>
+    </div>
+    <div class="mt-3">
+      <button type="submit" class="btn btn-primary">Generate Report</button>
+    </div>
+  </form>
+</div>
+@endsection
